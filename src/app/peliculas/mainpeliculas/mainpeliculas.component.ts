b84680ee02af82552e3d0feac6d67210ae585a3a
Gerardo Cordero <cordero.gerard@gmail.com>
2019-03-05 08:25:32 -0500

    edit and delete movies

--- src/app/peliculas/mainpeliculas/mainpeliculas.component.ts
+++ src/app/peliculas/mainpeliculas/mainpeliculas.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {PeliculasService} from '../peliculas.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-mainpeliculas',
@@ -10,7 +11,7 @@ import {PeliculasService} from '../peliculas.service';
 export class MainpeliculasComponent implements OnInit {
   private dataPeliculas;
 
-  constructor(private servicePeliculas: PeliculasService) {
+  constructor(private servicePeliculas: PeliculasService, private router: Router) {
   }
 
   ngOnInit() {
@@ -25,6 +26,22 @@ export class MainpeliculasComponent implements OnInit {
         localStorage.setItem('movies', JSON.stringify(this.dataPeliculas));
       });
     }
+  }
+
+  editMovie(item){
+    localStorage.setItem('movieEdit', JSON.stringify(item));
+    this.router.navigate(['peliculas/edit']);
+  }
+
+  deleteMovie(id){
+    console.log('id ', id);
+    const newArray = this.dataPeliculas.filter((item)=>{
+      if(item.id !== id){
+        return item;
+      }
+    });
+    this.dataPeliculas = [...newArray];
+    localStorage.setItem('movies', JSON.stringify(newArray));
   }
 
 }
